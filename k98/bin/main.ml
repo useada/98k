c0513fdf9c3c4ac0d4bb08223621ce5da637c156
lvlei <lvlei@inke.cn>
2021-11-26 16:53:03 +0800

    完整例子

--- k98/bin/main.ml
+++ k98/bin/main.ml
@@ -6,13 +6,13 @@ open Core
 
 let print_df df = Owl_pretty.pp_dataframe Format.std_formatter df
 
-let code = "002407.sz"
+let code = "600000.XSHG"
 
 let () =
   let open Or_error.Monad_infix in
   let data =
     K98.JQ.get_token "15011272962" "u123456A" >>= fun token ->
-    K98.JQ.get_price token code 10 "1d" "20211126" >>| fun df -> print_df df
+    K98.JQ.get_price token code 10 "1d" "2021-11-26" >>| fun df -> print_df df
   in
   match data with
   | Ok _ -> print_endline "ok"
